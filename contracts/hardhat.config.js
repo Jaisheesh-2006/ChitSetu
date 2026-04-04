3e9c1037b5ab08b291c22faddd9c65ba45631951
Vishnu Kothakapu <vishnukothakapu27@gmail.com>
2026-04-04 16:01:21 +0530

    feat(contracts): add config file and chitfund contract

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,6 +1,30 @@
-require("@nomicfoundation/hardhat-toolbox");
+import {HardhartUserConfig} from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+import "@nomicfoundation/hardhat-verify";
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.28",
+import * as dotenv from "dotenv";
+import { solidity } from "hardhat";
+import { url } from "node:inspector";
+
+dotenv.config({path: '../.env'});
+
+const config: HardhartUserConfig = {
+  solidity: "0.8.18",
+
+  networks:{
+    amoy:{
+      url: process.env.AMOY_RPC_URL || "",
+      accounts:process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY]:[],
+
+    },
+    localhost:{
+      url:"http://127.0.0.1:8545",
+      accounts:[
+        process.env.PRIVATE_KEY !== undefined ? process.env.PRIVATE_KEY : "",
+      ],
+    },
+  }
+
 };
+export default config;
